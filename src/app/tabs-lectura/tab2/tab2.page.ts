822ab69357ead6e3d3c62f88f2d3b414f8c15b50
KevinB1500 <kdbautis@espol.edu.ec>
2023-08-08 09:46:23 -0500

    Tab 2 completada

--- src/app/tabs-lectura/tab2/tab2.page.ts
+++ src/app/tabs-lectura/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab2Page implements OnInit {
 
-  constructor() { }
+  filter: any;
+  firstday: any;
+  lastday: any;
+  today: any;
+  reportes: any[] = [];
+  dataUser: any;
+  options: any [] = [];
+  selectedOption: any;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.dataUser = JSON.parse(sessionStorage.getItem('dataUser'));
+    this.today = new Date();
+    this.firstday = new Date(this.today.getFullYear(),this.today.getMonth(), 1);
+    this.lastday = new Date(this.today.getFullYear(),this.today.getMonth() + 1, 0);
+
+    this.filter = {
+      fecha_desde: moment(this.firstday).format('YYYY-MM-DD'),
+      fecha_hasta: moment(this.lastday).format('YYYY-MM-DD')
+    };
+
+    this.getOptions();
   }
+
+  getOptions(): void {
+    this.http.get(`http://localhost:3000/getMedidores`).subscribe(
+      (data: any) => {
+        this.options = data.data;
+      },
+      (error) => {
+        console.error('Ha ocurrido un error al obtener los datos', error);
+      }
+    );
+  }
+
+  getReportesFI(): void {
+    console.log('Botón presionado');
+    console.log(this.filter);
+    console.log(this.selectedOption);
+    this.http.post(`http://localhost:3000/getLecturasHistoricasPorMedidor`, {id: this.selectedOption, fechaInicio: this.filter.fecha_desde, fechaFin: this.filter.fecha_hasta}).subscribe(
+      (data: any) => {
+        this.reportes = data.data;
+      },
+      (error) => {
+        console.error('Ha ocurrido un error al obtener los datos', error);
+      }
+    );
+  }
+
+  getSelectedMedidorName(): string {
+    const medidor = this.options.find(option => option.id_medidor === this.selectedOption);
+    return medidor ? medidor.nombre : 'Medidor no seleccionado';
+  }
+  
 
 }
